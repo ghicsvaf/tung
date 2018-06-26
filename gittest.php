92303a061b0ebc87966f5b8526b9aa7c414ed391
ghicsvaf <ghicsvaf@gmail.com>
2018-06-26 15:39:59 +0700

    testgit

---
+++ gittest.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Request;
+
+
+class khachhangController extends Controller
+{
+    public function index()
+    {
+       return view('khachhang_index');
+    }
+    public function create()
+    {
+        return view('khachhang_create');
+    }
+    public function store(Request $request)
+    {
+        print_r($request->all());
+    }
+}
+?>
